6238eaf3d6b22eaf3e799d6581f7c40534c7e810
Oleg <brezitskyyoleg@gmail.com>
2021-03-23 01:30:35 +0100

    added memojis to side bar

--- source/conf.py
+++ source/conf.py
@@ -56,6 +56,8 @@ html_theme = "furo"
 html_theme_options = {
     "sidebar_hide_name": True,
     "navigation_with_keys": True,
+    "light_logo": "memojis.png",
+    "dark_logo": "memojis.png"
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
